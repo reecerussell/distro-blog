16aef3abe9636036fcdf3ba44e4a55727d3f265d
Reece Russell <reece@prominentmedia.com>
2020-06-03 22:59:45 +0100

    added password validation

--- password/service.go
+++ password/service.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"hash"
 	"log"
@@ -19,32 +20,87 @@ const (
 
 var encoding = base64.StdEncoding
 
+var (
+	// DefaultOptions are the service's default password validation options.
+	DefaultOptions = &Options{
+		RequiredLength:         6,
+		RequireUppercase:       true,
+		RequireLowercase:       true,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           true,
+		RequiredUniqueChars:    0,
+	}
+
+	// DefaultKeySize is the default key size.
+	DefaultKeySize = uint(256 / 8)
+
+	// DefaultSaltSize is the default salt size.
+	DefaultSaltSize = uint(128 / 8)
+
+	// DefaultHash is the default hash function.
+	DefaultHash = HashSHA256
+
+	// DefaultIterationCount is the default number of iterations
+	// used by the hasher.
+	DefaultIterationCount = uint(15000)
+)
+
 // Service is a high-level interface used to validate,
 // verify and hash passwords.
 type Service interface {
-	//Validate(password string) error
 	Hash(password string) string
 	Verify(password, hash string) bool
+	Validate(password string) error
 
 	// SetIterationCount(cnt uint)
 	// SetSaltSize(cnt uint)
 	// SetKeySize(cnt uint)
+	SetValidationOptions(opts *Options)
+}
+
+// Options contains a set of critrea that a
+// password has to hit, to be considered valid.
+type Options struct {
+	// RequiredLength is the minimum length a password has to be.
+	RequiredLength int
+
+	// RequireUppercase is a flag which demands at least one
+	// character to be uppercase.
+	RequireUppercase bool
+
+	// RequireLowercase is a flag which demands at least one
+	// character to be lowercase.
+	RequireLowercase bool
+
+	// RequireNonAlphanumeric is a flag which demands at least one
+	// character to be non alphanumeric.
+	RequireNonAlphanumeric bool
+
+	// RequireDigit is a flag which demands at least one
+	// character to be a digit.
+	RequireDigit bool
+
+	// RequiredUniqueChars is an integer value, that determines
+	// how many unique characters are required in a password.
+	RequiredUniqueChars int
 }
 
 func New() Service {
 	return &service{
-		iterationCount: uint(15000),
-		saltSize:       uint(128 / 8),
-		keySize:        uint(256 / 8),
-		hash:           HashSHA256,
+		iterationCount:    DefaultIterationCount,
+		saltSize:          DefaultSaltSize,
+		keySize:           DefaultKeySize,
+		hash:              DefaultHash,
+		validationOptions: DefaultOptions,
 	}
 }
 
 type service struct {
-	iterationCount uint
-	saltSize       uint
-	keySize        uint
-	hash           uint
+	iterationCount    uint
+	saltSize          uint
+	keySize           uint
+	hash              uint
+	validationOptions *Options
 }
 
 // SetIterationCount sets the number of iterations the hasher will use.
@@ -65,6 +121,11 @@ type service struct {
 // func (s *service) SetKeySize(cnt uint) {
 // 	s.keySize = cnt / 8
 // }
+
+// SetValidationOptions sets the service's validation options.
+func (s *service) SetValidationOptions(opts *Options) {
+	s.validationOptions = opts
+}
 
 func (s *service) Hash(password string) string {
 	salt := make([]byte, s.saltSize)
@@ -150,4 +211,104 @@ func getHashFunc(v uint) func() hash.Hash {
 	default:
 		panic(fmt.Errorf("unrecognized hash func: %d", v))
 	}
+}
+
+func (s *service) Validate(password string) error {
+	l := len(password)
+	if l < 1 {
+		return errors.New("password is required")
+	}
+
+	if l < s.validationOptions.RequiredLength {
+		return fmt.Errorf("password must be atleast %d characters long", s.validationOptions.RequiredLength)
+	}
+
+	var (
+		hasNonAlphanumeric bool
+		hasDigit           bool
+		hasLower           bool
+		hasUpper           bool
+		uniqueChars        []byte
+	)
+
+	for i := 0; i < l; i++ {
+		c := byte(password[i])
+
+		if !hasNonAlphanumeric && !isLetterOrDigit(c) {
+			hasNonAlphanumeric = true
+		}
+
+		if !hasDigit && isDigit(c) {
+			hasDigit = true
+		}
+
+		if !hasLower && isLower(c) {
+			hasLower = true
+		}
+
+		if !hasUpper && isUpper(c) {
+			hasUpper = true
+		}
+
+		d := true
+
+		for _, dc := range uniqueChars {
+			if dc == c {
+				d = false
+			}
+		}
+
+		if d {
+			uniqueChars = append(uniqueChars, c)
+		}
+	}
+
+	if s.validationOptions.RequireNonAlphanumeric && !hasNonAlphanumeric {
+		return errors.New("password requires an non-alphanumeric character")
+	}
+
+	if s.validationOptions.RequireDigit && !hasDigit {
+		return errors.New("password requires a digit")
+	}
+
+	if s.validationOptions.RequireLowercase && !hasLower {
+		return errors.New("password requires a lowercase letter")
+	}
+
+	if s.validationOptions.RequireUppercase && !hasUpper {
+		return errors.New("password requires an uppercase letter")
+	}
+
+	if s.validationOptions.RequiredUniqueChars >= 1 && len(uniqueChars) < s.validationOptions.RequiredUniqueChars {
+		return fmt.Errorf("password requires atleast %d unique characters", s.validationOptions.RequiredUniqueChars)
+	}
+
+	return nil
+}
+
+// isDigit returns a flag indicating whether the supplied character
+// is a digit - true if the character is a digit, otherwise false.
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+// isLower returns a flag indicating whether the supplied character is
+// a lower case ASCII letter - true if the character is a lower case
+// ASCII letter, otherwise false.
+func isLower(c byte) bool {
+	return c >= 'a' && c <= 'z'
+}
+
+// isUpper returns a flag indicating whether the supplied character is
+// an upper case ASCII letter - true if the character is an upper case ASCII
+// letter, otherwise false.
+func isUpper(c byte) bool {
+	return c >= 'A' && c <= 'Z'
+}
+
+// isLetterOrDigit returns a flag indicating whether the supplied character is
+// an ASCII letter or digit - true if the character is an ASCII letter or digit,
+// otherwise false.
+func isLetterOrDigit(c byte) bool {
+	return isUpper(c) || isLower(c) || isDigit(c)
 }
--- password/service_test.go
+++ password/service_test.go
@@ -72,3 +72,227 @@ func TestInvalidKeySize(t *testing.T) {
 		t.Errorf("expected password to be invalid")
 	}
 }
+
+func TestValidatePassword(t *testing.T) {
+	testPasswords := []string{
+		"Password_1234",
+		"Mypassword#2",
+	}
+
+	for i, p := range testPasswords {
+		err := serv.Validate(p)
+		if err != nil {
+			t.Errorf("validate[%d]: expected to be valid: %v", i, err)
+		}
+	}
+}
+
+func TestValidateEmptyPassword(t *testing.T) {
+	err := serv.Validate("")
+	if err == nil {
+		t.Errorf("expected an error, but got none")
+	}
+}
+
+func TestSetValidationOptions(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       true,
+		RequireLowercase:       true,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           true,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+	s := serv.(*service)
+
+	if s.validationOptions.RequireDigit != opts.RequireDigit {
+		t.Errorf("RequireDigit: expected %v, but got %v", opts.RequireDigit, s.validationOptions.RequireDigit)
+	}
+
+	if s.validationOptions.RequiredUniqueChars != opts.RequiredUniqueChars {
+		t.Errorf("RequiredUniqueChars: expected %d, but got %d", opts.RequiredUniqueChars, s.validationOptions.RequiredUniqueChars)
+	}
+
+	if s.validationOptions.RequiredLength != opts.RequiredLength {
+		t.Errorf("RequiredLength: expected %d, but got %d", opts.RequiredLength, s.validationOptions.RequiredLength)
+	}
+
+	if s.validationOptions.RequireUppercase != opts.RequireUppercase {
+		t.Errorf("RequireUppercase: expected %v, but got %v", opts.RequireUppercase, s.validationOptions.RequireUppercase)
+	}
+
+	if s.validationOptions.RequireLowercase != opts.RequireLowercase {
+		t.Errorf("RequireLowercase: expected %v, but got %v", opts.RequireLowercase, s.validationOptions.RequireLowercase)
+	}
+
+	if s.validationOptions.RequireNonAlphanumeric != opts.RequireNonAlphanumeric {
+		t.Errorf("RequireNonAlphanumeric: expected %v, but got %v", opts.RequireNonAlphanumeric, s.validationOptions.RequireNonAlphanumeric)
+	}
+}
+
+func TestValidateRequiresUppercasePassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       true,
+		RequireLowercase:       false,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           false,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "my test lowercase pass"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	opts.RequireUppercase = false
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}
+
+func TestValidateRequiresLowercasePassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       false,
+		RequireLowercase:       true,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           false,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "MY UPPERCASE PASSWORD"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	opts.RequireLowercase = false
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}
+
+func TestValidateShortPassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       false,
+		RequireLowercase:       true,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           false,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "short"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	pwd = "long password"
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}
+
+func TestValidateRequiresNonAlphanumericPassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       false,
+		RequireLowercase:       false,
+		RequireNonAlphanumeric: true,
+		RequireDigit:           false,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "nonalpha"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	err = serv.Validate("non-alpha")
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+
+	opts.RequireNonAlphanumeric = false
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}
+
+func TestValidateRequiresDigitPassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       false,
+		RequireLowercase:       false,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           true,
+		RequiredUniqueChars:    0,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "sdfghj"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	err = serv.Validate("3434hjk")
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+
+	opts.RequireDigit = false
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}
+
+func TestValidateRequiresUniqueCharsPassword(t *testing.T) {
+	opts := &Options{
+		RequiredLength:         6,
+		RequireUppercase:       false,
+		RequireLowercase:       false,
+		RequireNonAlphanumeric: false,
+		RequireDigit:           false,
+		RequiredUniqueChars:    2,
+	}
+
+	serv.SetValidationOptions(opts)
+
+	pwd := "aaaaaa"
+	err := serv.Validate(pwd)
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+
+	err = serv.Validate("abcdef")
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+
+	opts.RequiredUniqueChars = 0
+	err = serv.Validate(pwd)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+}

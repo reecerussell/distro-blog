c274b1b1aed0db765be7df75849c85a20b10d3f2
Reece Russell <reece@prominentmedia.com>
2020-06-06 23:28:12 +0100

    added user service

---
+++ domain/service/user_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/reecerussell/distro-blog/domain/model"
+	"github.com/reecerussell/distro-blog/domain/repository"
+	"github.com/reecerussell/distro-blog/libraries/result"
+)
+
+// UserService is used to provide the user domain with extra functionality,
+// such as validation methods which don't belong to the domain layer.
+type UserService struct {
+	repo repository.UserRepository
+}
+
+// NewUserService returns a new instance of UserService with the given repository.
+func NewUserService(repo repository.UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+// EnsureEmailIsUnique ensures the given user's email address is unique by
+// ensuring it has not be previously used in the database.
+func (s *UserService) EnsureEmailIsUnique(ctx context.Context, u *model.User) result.Result {
+	success, _, count, err := s.repo.CountByEmail(ctx, u).Deconstruct()
+	if !success {
+		return result.Failure(err)
+	}
+
+	if count.(int64) > 0 {
+		msg := fmt.Sprintf("The email address '%s' has already been taken.", u.Email())
+		return result.Failure(msg).WithStatusCode(http.StatusBadRequest)
+	}
+
+	return result.Ok()
+}
---
+++ domain/service/user_service_test.go
@@ -0,0 +1,92 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/reecerussell/distro-blog/domain/dto"
+	"github.com/reecerussell/distro-blog/domain/model"
+	"github.com/reecerussell/distro-blog/libraries/database"
+	"github.com/reecerussell/distro-blog/libraries/normalization"
+	"github.com/reecerussell/distro-blog/password"
+	"github.com/reecerussell/distro-blog/persistence/mysql"
+)
+
+var (
+	testConnString = os.Getenv("CONN_STRING")
+)
+
+func TestEnsureEmailIsUnique(t *testing.T) {
+	db := database.NewMySQL(testConnString)
+	repo := mysql.NewUserRepository(db)
+	serv := NewUserService(repo)
+	ctx := context.Background()
+
+	u := buildUser()
+	success, _, _, err := serv.EnsureEmailIsUnique(ctx, u).Deconstruct()
+	if !success {
+		t.Errorf("expected no error but got: %v", err)
+	}
+}
+
+func TestEnsureEmailIsUniqueWithNonUnique(t *testing.T) {
+	db := database.NewMySQL(testConnString)
+	repo := mysql.NewUserRepository(db)
+	serv := NewUserService(repo)
+	ctx := context.Background()
+
+	// seed user
+	u := buildUser()
+	success, _, _, err := repo.Add(ctx, u).Deconstruct()
+	if !success {
+		t.Errorf("expected no error but got: %v", err)
+	}
+
+	// check with existing user
+	success, _, _, err = serv.EnsureEmailIsUnique(ctx, u).Deconstruct()
+	if !success {
+		t.Errorf("expected no error but got: %v", err)
+	}
+
+	// check new user
+	u = buildUser()
+	success, _, _, _ = serv.EnsureEmailIsUnique(ctx, u).Deconstruct()
+	if success {
+		t.Errorf("expected an error but got nil")
+	}
+
+	// clean up
+	executeHelper("delete from `users`;")
+}
+
+func buildUser() *model.User {
+	cu := &dto.CreateUser{
+		Firstname: "John",
+		Lastname:  "Doe",
+		Email:     "john@doe.com",
+		Password:  "MyTestPassword123",
+	}
+	norm := normalization.New()
+	pwdServ := password.New()
+	u, err := model.NewUser(cu, pwdServ, norm)
+	if err != nil {
+		panic(err)
+	}
+
+	return u
+}
+
+func executeHelper(query string, args ...interface{}) {
+	db, err := sql.Open("mysql", testConnString)
+	if err != nil {
+		panic(fmt.Errorf("open: %v", err))
+	}
+
+	_, err = db.Exec(query, args...)
+	if err != nil {
+		panic(fmt.Errorf("exec: %v", err))
+	}
+}

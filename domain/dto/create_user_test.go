0397e4be30ebd4ef63a34c7f6345951529e8119b
Reece Russell <reece@prominentmedia.com>
2020-06-12 10:22:58 +0100

    changed package name and remove redundant test

--- domain/dto/create_user_test.go
+++ domain/dto/create_user_test.go
@@ -1,23 +1,21 @@
-package dto_test
+package dto
 
 import (
 	"encoding/json"
 	"testing"
-
-	"github.com/reecerussell/distro-blog/domain/dto"
 )
 
-var testCreateUser = &dto.CreateUser{
+var testCreateUser = &CreateUser{
 	Firstname: "john",
 	Lastname:  "doe",
 	Email:     "john@doe.com",
 	Password:  "password123",
 }
 
-func TestMarshal(t *testing.T) {
+func TestMarshalCreateUser(t *testing.T) {
 	bytes, err := json.Marshal(testCreateUser)
 	if err != nil {
-		t.Errorf("failed to marshal dto.CreateUser: %v", err)
+		t.Errorf("failed to marshal CreateUser: %v", err)
 		return
 	}
 
@@ -42,36 +40,5 @@ func TestMarshal(t *testing.T) {
 
 	if v := data["password"]; v != testCreateUser.Password {
 		t.Errorf("expected '%s' but got '%s'", testCreateUser.Password, v)
-	}
-}
-
-func TestUnmarshal(t *testing.T) {
-	bytes, err := json.Marshal(testCreateUser)
-	if err != nil {
-		t.Errorf("failed to marshal dto.CreateUser: %v", err)
-		return
-	}
-
-	var createUser dto.CreateUser
-	err = json.Unmarshal(bytes, &createUser)
-	if err != nil {
-		t.Errorf("failed to unmarshal dto.CreateUser: %v", err)
-		return
-	}
-
-	if createUser.Firstname != testCreateUser.Firstname {
-		t.Errorf("expected '%s' but got '%s'", testCreateUser.Firstname, createUser.Firstname)
-	}
-
-	if createUser.Lastname != testCreateUser.Lastname {
-		t.Errorf("expected '%s' but got '%s'", testCreateUser.Lastname, createUser.Lastname)
-	}
-
-	if createUser.Email != testCreateUser.Email {
-		t.Errorf("expected '%s' but got '%s'", testCreateUser.Email, createUser.Email)
-	}
-
-	if createUser.Password != testCreateUser.Password {
-		t.Errorf("expected '%s' but got '%s'", testCreateUser.Password, createUser.Password)
 	}
 }

979fa68a175d01681be65283919ecc9b73eafb11
Reece Russell <reece@prominentmedia.com>
2020-06-29 20:21:13 +0100

    added state boxes to audit

--- admin-client/src/app/app.module.ts
+++ admin-client/src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CreateUserComponent } from './create-user/create-user.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { EditUserComponent } from './edit-user/edit-user.component';
-import { LoginComponent } from './login/login.component';
-import { UserInfoComponent } from './user-info/user-info.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { CreateUserComponent } from "./create-user/create-user.component";
+import { UserListComponent } from "./user-list/user-list.component";
+import { EditUserComponent } from "./edit-user/edit-user.component";
+import { LoginComponent } from "./login/login.component";
+import { UserInfoComponent } from "./user-info/user-info.component";
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, CreateUserComponent, UserListComponent, EditUserComponent, LoginComponent, UserInfoComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        CreateUserComponent,
+        UserListComponent,
+        EditUserComponent,
+        LoginComponent,
+        UserInfoComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}

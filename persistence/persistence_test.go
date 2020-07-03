6640b1af75a4eac2708f248497d2d9e6a042436d
Reece Russell <reece@prominentmedia.com>
2020-07-03 16:43:45 +0100

    added a page repo method

--- persistence/persistence_test.go
+++ persistence/persistence_test.go
@@ -26,3 +26,24 @@ func TestUnsupportedUserRepo(t *testing.T) {
 
 	_ = NewUserRepository("")
 }
+
+func TestMySQLPageRepo(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("expected no panic, but got: %v", r)
+		}
+	}()
+
+	db := database.NewMySQL("")
+	_ = NewPageRepository(db)
+}
+
+func TestUnsupportedPageRepo(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("expected a panic")
+		}
+	}()
+
+	_ = NewPageRepository("")
+}

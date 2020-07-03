7c550b593995ea9ebc7344a80448a3f77828441b
Reece Russell <reece@prominentmedia.com>
2020-07-03 13:54:20 +0100

    added page datamodel

---
+++ domain/datamodel/page.go
@@ -0,0 +1,14 @@
+package datamodel
+
+import "database/sql"
+
+// Page is a data model used to read and write data
+// from a data source.
+type Page struct {
+	ID string
+	Title string
+	Description string
+	Content sql.NullString
+	IsBlog bool
+	IsActive bool
+}

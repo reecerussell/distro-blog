ee2c9475eb25bd0b9e30bbfc47f71c40077481e8
Reece Russell <reece@prominentmedia.com>
2020-07-04 21:55:14 +0100

    implemented the page list page

--- admin-client/src/app/page-list/page-list.component.ts
+++ admin-client/src/app/page-list/page-list.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
 
 @Component({
-  selector: 'app-page-list',
-  templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.scss']
+    selector: "app-page-list",
+    templateUrl: "./page-list.component.html",
+    styleUrls: ["./page-list.component.scss"],
 })
 export class PageListComponent implements OnInit {
+    pages: any = null;
+    searchTerm: string = "";
+    error: string = null;
+    loading: boolean = false;
 
-  constructor() { }
+    constructor(private api: ApiService) {}
 
-  ngOnInit(): void {
-  }
+    async ngOnInit() {
+        await this.loadPages();
+    }
 
+    async loadPages(): Promise<void> {
+        if (this.loading) {
+            return;
+        }
+
+        this.loading = true;
+
+        const res = await this.api.Pages.List();
+        if (res.ok) {
+            this.pages = res.data;
+        } else {
+            this.error = res.error;
+        }
+
+        this.loading = false;
+    }
+
+    getPages(): any {
+        const term = this.searchTerm.replace(/ +/g, " ").toLowerCase();
+        if (term.length < 1 || !this.pages) {
+            return this.pages;
+        }
+
+        return this.pages.filter((p) => {
+            const title = p.title.replace(/\s+/g, " ").toLowerCase();
+            const description = p.description
+                .replace(/\s+/g, " ")
+                .toLowerCase();
+
+            return title.indexOf(term) > -1 || description.indexOf(term) > -1;
+        });
+    }
 }

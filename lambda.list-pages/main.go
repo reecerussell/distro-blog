ff24d6dbf62eb7f69bdda726fa9fb717a1c2704a
Reece Russell <reece@prominentmedia.com>
2020-07-04 14:24:12 +0100

    added a pages list function

---
+++ lambda.list-pages/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	
+	"github.com/reecerussell/distro-blog/libraries/database"
+	"github.com/reecerussell/distro-blog/libraries/helper"
+	"github.com/reecerussell/distro-blog/persistence"
+	"github.com/reecerussell/distro-blog/usecase"
+)
+
+var pages usecase.PageUsecase
+
+func init() {
+	db := database.NewMySQL(os.Getenv("CONN_STRING"))
+	repo := persistence.NewPageRepository(db)
+	pages = usecase.NewPageUsecase(repo)
+}
+
+// handleList is a Lambda handler function used to handle incoming
+// APIGateway proxy requests to gather a list of pages.
+func handleList(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	ctx = helper.PopulateContext(ctx, req)
+	res := pages.ListPages(ctx)
+	return helper.Response(ctx, res, req), nil
+}
+
+func main() {
+	lambda.Start(handleList)
+}

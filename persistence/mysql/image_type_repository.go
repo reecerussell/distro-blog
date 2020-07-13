4b0dd2cafc1d95f4ec8329d72d77bf6f72bb8e15
Reece Russell <reece@prominentmedia.com>
2020-07-13 10:52:16 +0100

    add not found error handling

--- persistence/mysql/image_type_repository.go
+++ persistence/mysql/image_type_repository.go
@@ -31,15 +31,16 @@ func NewImageTypeRepository(db *database.MySQL) repository.ImageTypeRepository {
 
 func (r *imageTypeRepository) GetByName(ctx context.Context, name string) result.Result{
 	const query string = "CALL `get_image_type_by_name`(?);"
-	dm, err := r.db.Read(ctx, query, imageTypeReader, r.norm.Normalize(name))
-	if err != nil {
-		if err == sql.ErrNoRows {
-			msg := fmt.Sprintf("no image type exists with name '%s'", name)
-			return result.Failure(msg).WithStatusCode(http.StatusNotFound)
-		}
-
+	dm, err := r.db.Read(ctx, query, imageTypeReader, name)
+	if err != nil && err != sql.ErrNoRows {
 		logging.Error(err)
 		return result.Failure(errMsgImageTypeServerError)
+	}
+
+	if dm == nil || err == sql.ErrNoRows {
+		msg := fmt.Sprintf("no image type exists with name '%s'", name)
+		logging.Errorf("%s\n", msg)
+		return result.Failure(msg).WithStatusCode(http.StatusNotFound)
 	}
 
 	it := model.ImageTypeFromDataModel(dm.(*datamodel.ImageType))
